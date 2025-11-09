d47e258839caca9677af8a254268ee650dd56c3a
Hchaichi8 <Ghassen.HCHAICHI@esprit.tn>
2025-11-09 19:14:49 +0100

    SkiTest

---
+++ src/main/java/org/example/station_de_ski/StationDeSkiApplication.java
@@ -0,0 +1,13 @@
+package org.example.station_de_ski;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StationDeSkiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StationDeSkiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/example/station_de_ski/StationDeSkiApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.station_de_ski;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StationDeSkiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
